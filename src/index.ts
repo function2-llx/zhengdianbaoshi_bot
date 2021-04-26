986aa0826b7a246e555b1ee69b332b7e9ee4437e
function2 <function2@qq.com>
2021-04-26 19:54:41 +0800

    setup database

--- src/index.ts
+++ src/index.ts
@@ -14,30 +14,29 @@ function getHour() {
     date.setMinutes(date.getMinutes() + date.getTimezoneOffset() + 8 * 60);
 }
 
-bot.onText(/\/复读 (.+)/, (msg, match) => {
-    console.log(msg);
-    const chatId = msg.chat.id;
-    const text = match[1];
-    bot.sendMessage(chatId, `收到啦！你刚刚对我说：${text}`);
-});
-// typeorm.createConnection().then(async db => {
-//     console.log(`connected to database ${db.name}`);
-
-//     // bot.on('new_chat_members', async msg => {
-//     //     console.log(msg);
-//     //     if (msg.new_chat_members.find(member => member.id == botId)) {
-//     //         const chat = msg.chat;
-//     //         new Group({id: chat.id}).save();
-//     //         console.log(`加入${chat.title}(${chat.id})`);
-//     //         bot.sendMessage(chat.id, '我还在开发中，还不会报时呜呜呜 T T');
-//     //     }
-//     // });
+typeorm.createConnection().then(async db => {
+    console.log(`connected to database ${db.name}`);
     
-//     // bot.on('left_chat_member', async msg => {
-//     //     if (msg.left_chat_member.id == botId) {
-//     //         const chat = msg.chat;
-//     //         console.log(`离开${chat.title}(${chat.id})`);
-//     //         new Group({id: chat.id}).save();
-//     //     }
-//     // });
-// }).catch(error => console.log(error));
+    bot.onText(/\/复读 (.+)/, (msg, match) => {
+        const chatId = msg.chat.id;
+        const text = match[1];
+        bot.sendMessage(chatId, `收到啦！你刚刚对我说：${text}`);
+    });
+
+    bot.on('new_chat_members', async msg => {
+        if (msg.new_chat_members.find(member => member.id == botId)) {
+            const chat = msg.chat;
+            new Group({id: chat.id}).save();
+            console.log(`加入${chat.title}(${chat.id})`);
+            bot.sendMessage(chat.id, '我还在开发中，还不会报时呜呜呜 T T');
+        }
+    });
+    
+    bot.on('left_chat_member', async msg => {
+        if (msg.left_chat_member.id == botId) {
+            const chat = msg.chat;
+            console.log(`离开${chat.title}(${chat.id})`);
+            new Group({id: chat.id}).save();
+        }
+    });
+}).catch(error => console.log(error));
